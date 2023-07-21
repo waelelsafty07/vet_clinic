17cdcde3958ceefceb04e1c6bee5e01c2b08b22d
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-21 19:30:59 +0300

    Add indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -42,3 +42,12 @@ CREATE TABLE treatment_histories (
   medical_history__id INTEGER REFERENCES medical_histories(id),
   treatment_id INTEGER REFERENCES treatments(id),
 );
+
+  CREATE INDEX ON medical_histories (patient_id);
+  CREATE INDEX ON invoices (medical_history_id);
+  CREATE INDEX ON invoice_items (invoice_id);
+  CREATE INDEX ON invoice_items (treatment_id);
+  CREATE INDEX ON treatment_histories (medical_history__id);
+  CREATE INDEX ON treatment_histories (treatment_id);
+
+
